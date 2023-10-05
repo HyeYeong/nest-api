e4d46d5589e3088812bd00447708c7fd7d9cc400
HyeYeong <todzlfk1@gmail.com>
2023-10-05 15:57:00 +0900

    fix: modify main ts, app ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-// 데코레이터. 클래스 위의 함수이고, 클래스를 위 움직인다.
+// NOTE: @데코레이터. 클래스 위의 함수이고, 클래스를 위 움직인다.
 @Module({
   imports: [],
   controllers: [AppController],
+  // controllers가 하는 일? 일반적으로 url을 가져오고, 함수를 실행하는것
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // AppModule 은 모든 것의 루트 모듈 같은 거임
+  // 모듈은 어플리케이션의 일부분
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }

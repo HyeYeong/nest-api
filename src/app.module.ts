0b7235e60b83cc81024c18d0e5a4e4eb72a977a0
HyeYeong <todzlfk1@gmail.com>
2023-10-05 15:25:29 +0900

    feat: add note, and string

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 데코레이터. 클래스 위의 함수이고, 클래스를 위 움직인다.
 @Module({
   imports: [],
   controllers: [AppController],
